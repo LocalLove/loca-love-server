bb8c42317eccb2d3563e16734414e17ad4054507
tokyorider <noobfromrus920@gmail.com>
2021-01-04 21:15:55 +0700

    добавил сервис отправки сообщений на емейл

--- server/src/main/kotlin/com/localove/email/EmailService.kt
+++ server/src/main/kotlin/com/localove/email/EmailService.kt
@@ -1,5 +1,22 @@
 package com.localove.email
 
-class EmailService {
+import org.springframework.mail.MailSender
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
 
+@Service
+class EmailService(
+    private val sender: MailSender
+) {
+    @Async
+    fun sendMessage(to: String, subject: String, body: String) {
+        val message = SimpleMailMessage().apply {
+            setTo(to)
+            setSubject(subject)
+            setText(body)
+        }
+
+        sender.send(message)
+    }
 }
