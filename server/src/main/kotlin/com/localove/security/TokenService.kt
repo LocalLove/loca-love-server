4b23554915c85abd2705aef04d8699b780c755b4
tokyorider <noobfromrus920@gmail.com>
2021-01-07 00:27:04 +0700

    слил воедино два похожих класса

--- server/src/main/kotlin/com/localove/security/TokenService.kt
+++ server/src/main/kotlin/com/localove/security/TokenService.kt
@@ -1,22 +1,21 @@
 package com.localove.security
 
+import com.localove.exceptions.InvalidTokenException
 import com.localove.security.entities.Token
+import com.localove.security.entities.TokenRepository
 import com.localove.security.entities.User
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
-import java.time.Clock
 import java.time.Duration
 import java.time.LocalDateTime
 import java.util.*
 
 @Service
 class TokenService(
-    private val clock: Clock,
-
     @Value("\${loca-love.token.expiration}")
-    private val duration: Duration
+    private val expiration: Duration
 ) {
     @Transactional
     fun <T : Token> fillToken(tokenRepository: JpaRepository<T, Long>, user: User, token: T): T {
@@ -27,5 +26,26 @@ class TokenService(
         }
 
         return tokenRepository.save(token)
+    }
+
+    @Transactional
+    fun <R : TokenRepository> validateToken(tokenRepository: R, token: String): User {
+        val tokenValue = uuidFromString(token) ?: throw InvalidTokenException()
+        val emailToken = tokenRepository.findByValue(tokenValue) ?: throw InvalidTokenException()
+        val tokenLifetime = Duration.between(emailToken.creationTime, LocalDateTime.now())
+
+        if (tokenLifetime >= expiration) {
+            throw InvalidTokenException()
+        }
+
+        return emailToken.user
+    }
+}
+
+private fun uuidFromString(src: String): UUID? {
+    return try {
+        UUID.fromString(src)
+    } catch (e: IllegalArgumentException) {
+        null
     }
 }
