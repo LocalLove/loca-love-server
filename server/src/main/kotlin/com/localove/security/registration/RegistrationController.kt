93b86f6205e46b53e1eb6a27e9f9e558c36740e2
tokyorider <noobfromrus920@gmail.com>
2021-01-06 15:23:43 +0700

    Выделил проперти в конфиг

--- server/src/main/kotlin/com/localove/security/registration/RegistrationController.kt
+++ server/src/main/kotlin/com/localove/security/registration/RegistrationController.kt
@@ -1,15 +1,13 @@
 package com.localove.security.registration
 
 import com.localove.api.security.UserRegistrationDto
-import com.localove.security.EmailConfig
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RegistrationController(
-    private val registrationService: RegistrationService,
-    private val config: EmailConfig
+    private val registrationService: RegistrationService
 ) {
     @PostMapping("/sign-up")
     fun signUp(@RequestBody dto: UserRegistrationDto) {
