dce5026fcf865d63bfe3aadf7d968efbeb54991a
tokyorider <noobfromrus920@gmail.com>
2021-01-06 15:23:09 +0700

    Выделил проперти в конфиг

--- server/src/main/kotlin/com/localove/security/email/SecurityEmailService.kt
+++ server/src/main/kotlin/com/localove/security/email/SecurityEmailService.kt
@@ -1,9 +1,9 @@
 package com.localove.security.email
 
 import com.localove.email.EmailService
+import com.localove.security.EmailConfig
 import com.localove.util.LoggerProperty
 import com.localove.util.ResourceLoader
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import java.util.*
 
@@ -11,40 +11,30 @@ import java.util.*
 class SecurityEmailService(
     private val emailService: EmailService,
 
-    @Value("\${email.email-confirmation-url}")
-    private val emailConfirmationUrl: String,
-
-    @Value("\${email.password-restore-url}")
-    private val passwordRestoreUrl: String,
-
-    @Value("\${email.templates.email-confirmation-template-file-path}")
-    private val emailConfirmationTemplateFilePath: String,
-
-    @Value("\${email.templates.password-restore-template-file-path}")
-    private val passwordRestoreTemplateFilePath: String
+    private val config: EmailConfig
 ) {
     private val templateResolver = MessageTemplateResolver("{", "}")
 
     private val log by LoggerProperty()
 
-    fun sendEmailConfirmation(email: String, token: String) {
+    fun sendEmailConfirmation(email: String, uuid: UUID) {
         log.trace("Trying to send email confirmation message to $email")
 
-        val template = ResourceLoader.loadAsString(emailConfirmationTemplateFilePath)
+        val template = ResourceLoader.loadAsString(config.emailConfirmationTemplateFilePath)
         val substitutions = mapOf(
-            "url" to emailConfirmationUrl, "name" to email, "token" to token
+            "url" to config.emailConfirmationUrl, "name" to email, "uuid" to uuid.toString()
         )
         val body = templateResolver.resolve(template, substitutions)
 
         emailService.sendMessage(email, "Подтверждение почты", body)
     }
 
-    fun sendPasswordRestore(email: String, token: String) {
+    fun sendPasswordRestore(email: String, uuid: UUID) {
         log.trace("Trying to send password restore message to $email")
 
-        val template = ResourceLoader.loadAsString(passwordRestoreTemplateFilePath)
+        val template = ResourceLoader.loadAsString(config.passwordRestoreTemplateFilePath)
         val substitutions = mapOf(
-            "url" to passwordRestoreUrl, "name" to email, "token" to token
+            "url" to config.passwordRestoreUrl, "name" to email, "uuid" to uuid.toString()
         )
         val body = templateResolver.resolve(template, substitutions)
 
--- server/src/main/kotlin/com/localove/security/registration/RegistrationController.kt
+++ server/src/main/kotlin/com/localove/security/registration/RegistrationController.kt
@@ -1,13 +1,15 @@
 package com.localove.security.registration
 
 import com.localove.api.security.UserRegistrationDto
+import com.localove.security.EmailConfig
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class RegistrationController(
-    private val registrationService: RegistrationService
+    private val registrationService: RegistrationService,
+    private val config: EmailConfig
 ) {
     @PostMapping("/sign-up")
     fun signUp(@RequestBody dto: UserRegistrationDto) {
